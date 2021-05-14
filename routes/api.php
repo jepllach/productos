298c8644eec72958537e43a4dc2026d10b47123a
unknown <jepllach@gmail.com>
2021-05-13 22:07:09 -0400

    actualizacion en API

--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\productosController;
 use App\Http\Controllers\comentariosController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::post('productos/agregar', [productosController::class,'agregar']);
 Route::get('productos/{id}', [productosController::class,'mostrar']);
 Route::put('productos/{id}', [productosController::class,'actualizar']);
